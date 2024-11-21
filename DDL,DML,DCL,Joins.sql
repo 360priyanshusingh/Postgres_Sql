5d3a8e96473c2fdeed488f992d1ddb604fe24726
Priyanshu Sisodiya <upscpriyanshu06@gmail.com>
2024-11-21 17:42:31 +0530

    [Priyanshu] Added : All DDL DML DCL and Join Update

--- DDL,DML,DCL,Joins.sql
+++ DDL,DML,DCL,Joins.sql
@@ -1,3 +1,11 @@
+-- command                          uses
+-- \l	                        List all databases
+-- \c dbname	                Connect to a database
+-- \dt	                        List all tables in the current database
+-- \d                           tablename Describe a table (list columns and details)
+-- \q	                        Quit the psql terminal
+
+
 -- 1 DDL(data defination language) :-
 create database order_service;
 create database student
@@ -12,17 +20,7 @@ drop table student
 insert into student_table(rollNumber,FirstName,LastName) values(3,'Divya','Sisodiya3');
 
 COMMENT "priyanshu sisodiya" ON TABLE student2;
-RENAME TABLE student1 TO student2;
 
-alter table student2 add column id int 
-
-alter table student2 drop column id
-
-alter table student2 rename column  id to _id 
-
-select * from student2  
-
-update student2 set _id=3 where rollNumber=3
 
 -- 2 DML(data manuplation language) :- 
 
@@ -31,11 +29,28 @@ insert into student_Table(rollNumber,FirstName,LastName) values('1','Priyanshu',
 
 INSERT INTO t_orders(id,order_number) values(6,'826shw653g6g7dve7dvw7'),(7,'826shw653g6g7dve7dvw7'),(8,'8237hdgd653g7sg57g7gsf537')
 insert into t_orders values(10,'8dhy736dge64gd763gdye6');
-update t_orders set order_number='837hde6ts6fffff'where  id=1 ;
+update t_orders set order_number='837hde6ts6fffff' where  id=1 ;
 delete from t_orders where id=6
+RENAME TABLE student1 TO student2;
+
+alter table student2 add column id int 
+alter table student2 drop column id
+alter table student2 rename column  id to _id 
+select * from student2  
+
+update student2 set _id=3 where rollNumber=3
 
 -- 3 data query language (DQL):-
+
 select * from student_table 
+
+-- for showing all database
+SELECT datname FROM pg_database;
+
+-- for showing all colunm name of perticular table name
+SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_name = 'student_table';
+
+
 
 
 -- 3 joins :- 
